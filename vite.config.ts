b1a7975860fe2a66cb43873de1e58a26295979de
nbogie <neillbogie@googlemail.com>
2023-01-09 22:18:51 +0000

    add eslint for vite and react and ts. wip.

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,11 @@
 // See https://github.com/vitest-dev/vitest/blob/main/examples/react-testing-lib/vite.config.ts
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import eslint from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), eslint()],
   test: {
     globals: true,
     environment: 'jsdom',
