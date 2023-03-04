b9561e5cb75e9655f8df461e2b3aaa1de742608b
nbogie <neillbogie@googlemail.com>
2023-03-04 04:14:24 +0000

    correct path to test-utils

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -1,10 +1,10 @@
 import App from "./App";
-import { render, screen } from "./utils/test-utils";
+import { render, screen } from "../utils/test-utils";
 
 //An example of using react-testing-library
 test("Input", async () => {
-  it("should render the input", () => {
-    render(<App />);
-    expect(screen.getByText("Cool thing to test")).toBeInTheDocument();
-  });
+    it("should render the input", () => {
+        render(<App />);
+        expect(screen.getByText("Cool thing to test")).toBeInTheDocument();
+    });
 });
