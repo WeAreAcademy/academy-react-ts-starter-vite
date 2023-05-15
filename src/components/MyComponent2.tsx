d693ba4c3f1c4e08aa3ef62e2c6c5823b846db48
nbogie <neillbogie@googlemail.com>
2023-05-15 18:30:50 +0100

    add more files for lint rules testing

---
+++ src/components/MyComponent2.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+const [bad, setBad] = useState(3);
+
+function MyComponent2(): JSX.Element {
+    const x = true;
+    if (x) {
+        const [x, setX] = useState(10);
+    }
+    return <div>second component</div>;
+}
+
+function myUnusedFn(a: number): number {
+    return a * a;
+}
