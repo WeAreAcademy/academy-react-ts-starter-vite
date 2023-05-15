a7732ad33acebd82b110d2d555b2746bed50b6f1
nbogie <neillbogie@googlemail.com>
2023-05-15 22:12:59 +0100

    add vite-plugin-checker to run ts and lint

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,20 @@
 // See https://github.com/vitest-dev/vitest/blob/main/examples/react-testing-lib/vite.config.ts
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { checker } from "vite-plugin-checker";
 import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     // currently seems to require you manually reload the app ?
-    plugins: [eslint(), react()],
+    plugins: [
+        eslint(),
+        react(),
+        checker({
+            typescript: true,
+            eslint: { lintCommand: "lint src" },
+        }),
+    ],
     test: {
         globals: true,
         environment: "jsdom",
