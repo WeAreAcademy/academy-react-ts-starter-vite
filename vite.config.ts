f9af4edf787eb79e5273145449e1d21c6f528b59
nbogie <neillbogie@googlemail.com>
2023-05-15 22:52:46 +0100

    remove vite-plugin-eslint import in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,12 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { checker } from "vite-plugin-checker";
-import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     // currently seems to require you manually reload the app ?
     plugins: [
-        // eslint(),
+        // eslint(),  //import eslint from "vite-plugin-eslint"
         react(),
         checker({
             typescript: true,
