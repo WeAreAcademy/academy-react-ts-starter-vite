48adc3f1ffa6ef4a12ec25b90a08be6537d15117
nbogie <neillbogie@googlemail.com>
2023-05-15 22:00:12 +0100

    formatting only

--- vite.config.ts
+++ vite.config.ts
@@ -2,20 +2,20 @@
 /// <reference types="vite/client" />
 
 // See https://github.com/vitest-dev/vitest/blob/main/examples/react-testing-lib/vite.config.ts
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-import eslint from 'vite-plugin-eslint'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // currently seems to require you manually reload the app ?
-  plugins: [eslint(), react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/test/setup.ts',
-    // you might want to disable it, if you don't have tests that rely on CSS
-    // since parsing CSS is slow
-    css: true,
-  },
-})
+    // currently seems to require you manually reload the app ?
+    plugins: [eslint(), react()],
+    test: {
+        globals: true,
+        environment: "jsdom",
+        setupFiles: "./src/test/setup.ts",
+        // you might want to disable it, if you don't have tests that rely on CSS
+        // since parsing CSS is slow
+        css: true,
+    },
+});
