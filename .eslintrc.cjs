ae0b05c909d1dae9c00e7f496d9af5ed50324ac3
nbogie <neillbogie@googlemail.com>
2023-05-15 18:33:41 +0100

    .eslintrc.cjs: for now don't include eslint-config-prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -18,7 +18,8 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     // This disables the formatting rules in ESLint that Prettier is going to be responsible for handling.
     // Make sure it's always the last config, so it gets the chance to override other configs.
-    'eslint-config-prettier',
+
+    // 'eslint-config-prettier',
   ],
   settings: {
     react: {
