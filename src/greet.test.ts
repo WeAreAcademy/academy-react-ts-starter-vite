8709bd423e4939b6372c14a822505f1362676480
nbogie <neillbogie@googlemail.com>
2023-05-16 01:29:30 +0100

    describe test

--- src/greet.test.ts
+++ src/greet.test.ts
@@ -1,6 +1,6 @@
 import { greet } from "./greet";
 
-test("Thing", () => {
+test("greet returns a string, greeting the passed name", () => {
     expect(greet("Zaphod")).toEqual("Hello, Zaphod");
     expect(greet("Mo")).toEqual("Hello, Mo");
 });
