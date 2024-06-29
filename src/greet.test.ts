a3e3383aca60a1e585b323d2bd42a1559b355d79
nbogie <neillbogie@googlemail.com>
2024-06-29 11:00:23 +0100

    add demo of chai asserts

--- src/greet.test.ts
+++ src/greet.test.ts
@@ -1,6 +1,15 @@
 import { greet } from "./greet";
 
+//docs: https://jestjs.io/docs/expect
 test("greet returns a string, greeting the passed name", () => {
     expect(greet("Zaphod")).toEqual("Hello, Zaphod");
     expect(greet("Mo")).toEqual("Hello, Mo");
+});
+
+//docs: https://www.chaijs.com/api/assert/
+test("demonstrating chai 'assert' library", () => {
+    assert.equal(greet("Zaphod"), "Hello, Zaphod");
+    assert.equal(greet("Mo"), "Hello, Mo");
+
+    assert.deepEqual({ b: [10, 20], a: 10 }, { a: 10, b: [10, 20] });
 });
